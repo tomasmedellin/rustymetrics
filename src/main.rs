1c21db0fb49fabe18171cb94f27f2905389542b4
tomasmedellin <tomasmedellinvelasco@gmail.com>
2023-10-23 21:32:03 -0700

    Functionality for detailed metric reporting

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ use crossterm::event::{self, KeyCode};
 enum Section {
     Welcome,
     Metrics,
-    Instructions,
     DetailedMetrics,
     CPUDetails,
     MemoryDetails,
@@ -41,7 +40,6 @@ fn main() {
         if event::poll(Duration::from_millis(10)).unwrap() {
             match event::read().unwrap() {
                 event::Event::Key(event) => {
-                    needs_redraw = true;
                     match event.code {
                         KeyCode::Char('q') => {
                             crossterm::terminal::disable_raw_mode().unwrap();
@@ -50,34 +48,42 @@ fn main() {
                         }
                         KeyCode::Up => {
                             current_section = match current_section {
-                                Section::Instructions => Section::Metrics,
                                 Section::Metrics => Section::Welcome,
-                                Section::Welcome => Section::Welcome,
-                                Section::DetailedMetrics => Section::BatteryDetails,
+                                Section::DetailedMetrics => Section::Metrics,
                                 Section::CPUDetails => Section::DetailedMetrics,
                                 Section::MemoryDetails => Section::CPUDetails,
                                 Section::BatteryDetails => Section::MemoryDetails,
+                                _ => current_section,
                             };
+                            needs_redraw = true;
                         }
                         KeyCode::Down => {
                             current_section = match current_section {
                                 Section::Welcome => Section::Metrics,
-                                Section::Metrics => Section::Instructions,
-                                Section::Instructions => Section::Instructions,
+                                Section::Metrics => Section::DetailedMetrics,
                                 Section::DetailedMetrics => Section::CPUDetails,
                                 Section::CPUDetails => Section::MemoryDetails,
                                 Section::MemoryDetails => Section::BatteryDetails,
-                                Section::BatteryDetails => Section::DetailedMetrics,
+                                _ => current_section,
                             };
+                            needs_redraw = true;
                         }
-                        KeyCode::Enter => {
-                            match current_section {
-                                Section::Metrics => current_section = Section::DetailedMetrics,
-                                Section::DetailedMetrics => current_section = Section::CPUDetails,
-                                Section::CPUDetails => current_section = Section::MemoryDetails,
-                                Section::MemoryDetails => current_section = Section::BatteryDetails,
-                                Section::BatteryDetails => current_section = Section::Welcome,
-                                _ => {}
+                        KeyCode::Char('1') => {
+                            if let Section::DetailedMetrics = current_section {
+                                current_section = Section::CPUDetails;
+                                needs_redraw = true;
+                            }
+                        }
+                        KeyCode::Char('2') => {
+                            if let Section::DetailedMetrics = current_section {
+                                current_section = Section::MemoryDetails;
+                                needs_redraw = true;
+                            }
+                        }
+                        KeyCode::Char('3') => {
+                            if let Section::DetailedMetrics = current_section {
+                                current_section = Section::BatteryDetails;
+                                needs_redraw = true;
                             }
                         }
                         _ => {}
@@ -109,12 +115,51 @@ fn main() {
                 let welcome = Paragraph::new("NovaMetrics").block(block).wrap(Wrap { trim: true });
                 f.render_widget(welcome, chunks[0]);
 
-                let metrics = format!(
-                    "CPU: {:.2}%\nMemory: {} KB\nBattery: {}%",
-                    get_cpu_usage(),
-                    get_used_memory(),
-                    get_battery_percentage()
-                );
+                let metrics = match current_section {
+                    Section::Metrics => format!(
+                        "CPU: {:.2}%\nMemory: {} KB\nBattery: {}%",
+                        get_cpu_usage(),
+                        get_used_memory(),
+                        get_battery_percentage()
+                    ),
+                    Section::DetailedMetrics => "Detailed Metrics:\n1. CPU\n2. Memory\n3. Battery".to_string(),
+                    Section::CPUDetails => {
+                        let mut system = System::new_all();
+                        system.refresh_all();
+                        let processor = &system.processors()[0];
+                        format!(
+                            "CPU Details:\n- Name: {}\n- Frequency: {} MHz\n- Usage: {:.2}%",
+                            processor.brand(),
+                            processor.frequency(),
+                            processor.cpu_usage()
+                        )
+                    }
+                    Section::MemoryDetails => {
+                        let mut system = System::new_all();
+                        system.refresh_all();
+                        format!(
+                            "Memory Details:\n- Total: {} KB\n- Used: {} KB\n- Free: {} KB",
+                            system.total_memory(),
+                            system.used_memory(),
+                            system.free_memory()
+                        )
+                    }
+                    Section::BatteryDetails => {
+                        let manager = Manager::new().unwrap();
+                        if let Some(battery) = manager.batteries().unwrap().next() {
+                            let battery = battery.unwrap();
+                            format!(
+                                "Battery Details:\n- Status: {:?}\n- Energy: {} Wh\n- Temperature: {}°C",
+                                battery.state(),
+                                battery.energy().get::<battery::units::energy::watt_hour>(),
+                                battery.temperature().map_or(0.0, |t| t.get::<battery::units::thermodynamic_temperature::degree_celsius>())
+                            )
+                        } else {
+                            "Battery Details:\n- No battery information available.".to_string()
+                        }
+                    }
+                    _ => "".to_string(),
+                };
                 let block = match current_section {
                     Section::Metrics => Block::default().borders(Borders::ALL).title("Metrics").style(Style::default().fg(Color::Yellow)),
                     _ => Block::default().borders(Borders::ALL).title("Metrics"),
@@ -123,10 +168,7 @@ fn main() {
                 f.render_widget(metrics_widget, chunks[1]);
 
                 let instructions = "Use arrow keys to navigate. Press 'q' to quit.";
-                let block = match current_section {
-                    Section::Instructions => Block::default().borders(Borders::ALL).title("Instructions").style(Style::default().fg(Color::Yellow)),
-                    _ => Block::default().borders(Borders::ALL).title("Instructions"),
-                };
+                let block = Block::default().borders(Borders::ALL).title("Instructions");
                 let instructions_widget = Paragraph::new(instructions).block(block).wrap(Wrap { trim: true });
                 f.render_widget(instructions_widget, chunks[2]);
             }).unwrap();
